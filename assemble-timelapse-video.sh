cd440ecaeb006093da5468bc90d27e09d8f451af
Emil Lundberg <emil@emlun.se>
2021-09-26 23:42:32 +0200

    Add ffmpeg script

---
+++ assemble-timelapse-video.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+IMG_DIR="${1%%/}"
+
+print_usage() {
+  echo "Usage: $0 <TIMELAPSE DIR>"
+  echo
+  echo "Assemble *-replay.png files in <TIMELAPSE_DIR> into a video file."
+  exit 1
+}
+
+if [[ -z "${IMG_DIR}" ]]; then
+  print_usage
+fi
+
+FRAME_RATE=30
+IMG_PATTERN="%08d-replay.png"
+
+ffmpeg -f image2 -framerate "${FRAME_RATE}" -i "${IMG_DIR}/${IMG_PATTERN}" -c:v libx265 "${IMG_DIR}/timelapse.mkv"
