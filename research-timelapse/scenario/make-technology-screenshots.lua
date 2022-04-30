2cbba7a7d88771b4663b7e49d2fbb5cdd9911a17
Emil Lundberg <emil@emlun.se>
2022-05-01 01:19:58 +0200

    Make research-timelapse resource gen scenario more reliable

--- research-timelapse/scenario/make-technology-screenshots.lua
+++ research-timelapse/scenario/make-technology-screenshots.lua
@@ -30,7 +30,7 @@ function record_technologies(and_then)
   local screenshot_done = false
 
   return function (event)
-      if event.tick % 2 == 0 then
+      if event.tick % 4 == 0 then
         if force.current_research then
           game.take_screenshot{
             path = string.format("research-timelapse/technology/%s.png", force.current_research.name),
@@ -41,7 +41,7 @@ function record_technologies(and_then)
           screenshot_done = true
         end
 
-      else
+      elseif event.tick % 4 == 2 then
         if force.current_research then
           if screenshot_done then
             force.research_progress = 1
@@ -52,7 +52,7 @@ function record_technologies(and_then)
             i = (i + 1) % #tech_names
             force.research_queue = { tech_names[i + 1] }
           else
-            script.on_event(defines.events.on_tick, nil)
+            script.on_nth_tick(2, nil)
             game.print("Done recording technologies!")
 
             if and_then then
@@ -79,25 +79,15 @@ function record_progress(and_then)
     force.research_queue = { "automation" }
     force.research_progress = 0
 
-    local count_100 = 1
-    local record_100 = function (event)
-      count_100 = count_100 + 1
-
-      if count_100 == 25 then
-        game.take_screenshot{path = "research-timelapse/progress-bar/progress-1000.png", show_gui=true, force_render=true}
-        game.print("Done recording progress!")
-        script.on_event(defines.events.on_tick, nil)
-        if and_then then
-          and_then()
-        end
-      end
-    end
-
     local record = function (event)
       if force.current_research then
         local p = math.floor(force.research_progress * 1000)
         if take_next_screenshot then
-          game.take_screenshot{path = string.format("research-timelapse/progress-bar/progress-%04d.png", p), show_gui=true, force_render=true}
+          game.take_screenshot{
+            path = string.format("research-timelapse/progress-bar/progress-%04d.png", p),
+            show_gui=true,
+            force_render=true,
+          }
           take_next_screenshot = false
           last_p = p
         elseif p ~= last_p then
@@ -105,17 +95,35 @@ function record_progress(and_then)
         else
           force.research_progress = math.min(1, force.research_progress + 0.0004)
         end
-
-      else
-        script.on_event(defines.events.on_tick, nil)
-        script.on_event(defines.events.on_tick, record_100)
       end
     end
 
     set_timeout(
       5,
       function ()
-        script.on_event(defines.events.on_tick, record)
+        script.on_nth_tick(2, record)
+      end
+    )
+
+    script.on_event(
+      defines.events.on_research_finished,
+      function (event)
+        set_timeout(
+          1,
+          function()
+            game.take_screenshot{
+              path = "research-timelapse/progress-bar/progress-1000.png",
+              show_gui=true,
+              force_render=true,
+            }
+            game.print("Done recording progress!")
+            script.on_nth_tick(2, nil)
+            script.on_event(defines.events.on_research_finished, nil)
+            if and_then then
+              and_then()
+            end
+          end
+        )
       end
     )
   end
@@ -123,8 +131,8 @@ end
 
 return {
   run = function ()
-    script.on_event(
-      defines.events.on_tick,
+    script.on_nth_tick(
+      2,
       record_technologies(
         record_progress(
           function ()
