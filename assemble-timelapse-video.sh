644faf48f71d7a5a1277aed1f47433cca744ef99
Emil Lundberg <emil@emlun.se>
2022-05-09 00:02:43 +0200

    Add OUTPUT_DIR argument to assemble-timelapse scripts

--- assemble-timelapse-video.sh
+++ assemble-timelapse-video.sh
@@ -3,12 +3,19 @@
 # Exit on error
 set -e
 
+FRAME_RATE=30
+IMG_PATTERN="%08d-base.png"
+
 IMG_DIR="${1%%/}"
+OUTPUT_DIR="${2:-.}"
+OUTPUT_DIR="${OUTPUT_DIR%%/}"
 
 print_usage() {
-  echo "Usage: $0 <TIMELAPSE DIR>"
+  echo "Usage: $0 <TIMELAPSE DIR> [OUTPUT_DIR]"
   echo
   echo "Assemble *-base.png files in <TIMELAPSE_DIR> into a video file."
+  echo
+  echo "OUTPUT_DIR defaults to the current directory."
   exit 1
 }
 
@@ -16,7 +23,4 @@ if [[ -z "${IMG_DIR}" ]]; then
   print_usage
 fi
 
-FRAME_RATE=30
-IMG_PATTERN="%08d-base.png"
-
-ffmpeg -f image2 -framerate "${FRAME_RATE}" -i "${IMG_DIR}/${IMG_PATTERN}" -c:v libx265 "${IMG_DIR}/timelapse.mkv"
+ffmpeg -f image2 -framerate "${FRAME_RATE}" -i "${IMG_DIR}/${IMG_PATTERN}" -c:v libx265 "${OUTPUT_DIR}/timelapse.mkv"
--- assemble-timelapse-with-rocket.sh
+++ assemble-timelapse-with-rocket.sh
@@ -3,13 +3,23 @@
 # Exit on error
 set -e
 
+BASE_FRAME_RATE=30
+BASE_IMG_PATTERN="%08d-base.png"
+ROCKET_FRAME_RATE=60
+ROCKET_IMG_PATTERN="%08d-rocket.png"
+ROCKET_IMG_GLOB="*-rocket.png"
+
 IMG_DIR="${1%%/}"
-ROCKET_FRAME_OFFSET="$2"
+OUTPUT_DIR="${2:-.}"
+OUTPUT_DIR="${OUTPUT_DIR%%/}"
+ROCKET_FRAME_OFFSET=$(basename $(find "${IMG_DIR}" -type f -name "${ROCKET_IMG_GLOB}" | head -n 1) | cut -d '-' -f 1)
 
 print_usage() {
-  echo "Usage: $0 <TIMELAPSE DIR> <ROCKET FRAME OFFSET>"
+  echo "Usage: $0 <TIMELAPSE DIR> [OUTPUT_DIR]"
   echo
   echo "Assemble *-base.png and *-rocket.png files (one segment of each) in <TIMELAPSE_DIR> into a video file."
+  echo
+  echo "OUTPUT_DIR defaults to the current directory."
   exit 1
 }
 
@@ -17,16 +27,12 @@ if [[ -z "${IMG_DIR}" ]]; then
   print_usage
 fi
 if [[ -z "${ROCKET_FRAME_OFFSET}" ]]; then
-  print_usage
+  echo "Failed to compute index of first rocket frame."
+  exit 1
 fi
-
-BASE_FRAME_RATE=30
-BASE_IMG_PATTERN="%08d-base.png"
-ROCKET_FRAME_RATE=60
-ROCKET_IMG_PATTERN="%08d-rocket.png"
 
 ffmpeg \
   -f image2 -framerate "${BASE_FRAME_RATE}" -i "${IMG_DIR}/${BASE_IMG_PATTERN}" \
   -f image2 -framerate "${ROCKET_FRAME_RATE}" -start_number "${ROCKET_FRAME_OFFSET}" -i "${IMG_DIR}/${ROCKET_IMG_PATTERN}" \
   -filter_complex "[0:v] [1:v] concat=n=2 [out]" \
-  -map '[out]' -c:v libx265 "${IMG_DIR}/timelapse-rocket.mkv"
+  -map '[out]' -c:v libx265 "${OUTPUT_DIR}/timelapse-rocket.mkv"
